cb6fbcb80415c36325ae53dcfc9dfdcc766f60f1
Bae Ji Won <qowhxk@naver.com>
2024-07-31 23:14:43 +0900

    관심상품 희망 최저가 업데이트 API

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.service.ProductService;
@@ -13,8 +14,17 @@ public class ProductController {
 
     private final ProductService productService;
 
+    // 상품 담기
     @PostMapping("/products")
     public ProductResponseDto cteateProduct(@RequestBody ProductRequestDto requestDto) {
         return productService.createProduct(requestDto);
     }
+
+    // 상품 최저가 가격 설정
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        return productService.updateProduct(id, requestDto);
+    }
+
+
 }
